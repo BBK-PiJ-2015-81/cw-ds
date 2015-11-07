bdf150d0b69cb938c4e80b87037edc705bcc4727
BBK-PiJ-2015-81 <abrait02@dcs.bbk.ac.uk>
2015-11-07 01:04:03 +0000

    Completed LinkedList.

--- LinkedList.java
+++ LinkedList.java
@@ -27,56 +27,56 @@ public class LinkedList implements List {
 
 	public ReturnObject get(int index){
 		
-		//Not sure why this objReturned needs to be initialised when the others didn't. Investigate.
+		//Not sure why this objReturned needs to be initialised. Investigate.
 		ReturnObject objReturned = null;
 		
 		if (isEmpty() == true){
 			objReturned = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
 		} else if (index < 0 || index >= size()) {
 			objReturned = new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else if (index == 0) {
+			objReturned = new ReturnObjectImpl(firstItem);
+		} else if (index == (listSize - 1)) {
+			objReturned = new ReturnObjectImpl(lastItem);
 		} else {
-			if (index == 0) {
-				objReturned = new ReturnObjectImpl(firstItem);
-			} else if (index == (listSize - 1)) {
-				objReturned = new ReturnObjectImpl(lastItem);
-			} else {
-				ListNode tempNode = firstItem;
-				for (int i = 0; i < index; i++){
+			ListNode positionNode = firstItem;
+			for (int i = 0; i < index; i++){
 					
-					// move through the list.
-					tempNode = tempNode.getNext();
-					objReturned = new ReturnObjectImpl(tempNode);
+			// move through the list to our desired index.
+			positionNode = positionNode.getNext();
+			objReturned = new ReturnObjectImpl(positionNode);
 				}
 			} 			
-		} 
 		return objReturned;
-	}
+		} 
+		
+	
 	
 	
 	public ReturnObject remove(int index){
 		
-		ReturnObject objReturned;
+		ReturnObject objReturned = null;
 		
 		if (index < 0 || index >= size()) {
 			objReturned = new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
 		} else {
 			objReturned = get(index);
-			// If we remove the only object reset firstItem, lastItem.
+			// If we remove the only object then reset firstItem, lastItem.
 			if (size() == 1) {
 				firstItem = null; 
 				lastItem = null;
 			} else {
 				//Move to one item before the item being removed.
-				ListNode tempNode = firstItem;
+				ListNode positionNode = firstItem;
 				for (int i = 0; i < (index - 1); i++) {
-					tempNode = tempNode.getNext();
+					positionNode = positionNode.getNext();
 				}
 				// If the item being removed is the lastItem, make the one before the new lastItem;
-				if (tempNode.getNext().getNext() == null){
-					tempNode.setNext(null);
+				if (positionNode.getNext().getNext() == null){
+					positionNode.setNext(null);
 				} else {
 				// Point the item before the item being removed to the item after that.	
-					tempNode.setNext(tempNode.getNext().getNext());
+					positionNode.setNext(positionNode.getNext().getNext());
 				}
 			}
 		}
@@ -86,40 +86,59 @@ public class LinkedList implements List {
 		
 	}
 
-	/**
-	 * Adds an element to the list, inserting it at the given
-	 * position. The indeces of elements at and after that position
-	 * must be updated accordignly.
-	 * 
-	 * If the index is negative or greater or equal than the size of
-	 * the list, then an appropriate error must be returned.
-	 * 
-	 * If a null object is provided to insert in the list, the
-	 * request must be ignored and an appropriate error must be
-	 * returned.
-	 * 
-	 * @param index the position at which the item should be inserted in
-	 *              the list
-	 * @param item the value to insert into the list
-	 * @return an ReturnObject, empty if the operation is successful
-	 *         or containing an appropriate error message otherwise
-	 */
-	public ReturnObject add(int index, Object item){
-		return null;
-	}
 
-	/**
-	 * Adds an element at the end of the list.
-	 * 
-	 * If a null object is provided to insert in the list, the
-	 * request must be ignored and an appropriate error must be
-	 * returned.
-	 * 
-	 * @param item the value to insert into the list
-	 * @return an ReturnObject, empty if the operation is successful
-	 *         or containing an appropriate error message otherwise
-	 */
+	public ReturnObject add(int index, Object item){
+		
+		ReturnObject objReturned = null;
+		
+		if (index < 0 || index >= size()) {
+			objReturned = new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else if (item == null) {
+			objReturned = new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		} else {
+			ListNode positionNode = null;
+			ListNode addedNode = null;
+			
+			if (isEmpty() == true) {
+				firstItem = addedNode;
+				lastItem = firstItem;			
+			} else if (index == 0) {
+				// Put in front of the current first item.
+				positionNode.setNext(firstItem);
+				// Make the new first item.
+				firstItem = positionNode;
+			} else {
+				// Go to the item before where we want to insert
+				positionNode = firstItem;
+				for (int i = 0; i < (index - 1); i++) {
+					positionNode.setNext(positionNode.getNext());
+				}
+				
+				addedNode.setNext(positionNode.getNext());
+				positionNode.setNext(addedNode);
+			}
+		}
+		listSize = listSize + 1;
+		return objReturned;
+	}
+	
+	
 	public ReturnObject add(Object item){
-		return null;
+		
+		ReturnObject objReturned = null;
+		ListNode addedNode = null;
+		
+		if (item == null) {
+			objReturned = new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		} else if (isEmpty() == true) {
+			firstItem = addedNode;
+			lastItem = firstItem;	
+		} else {
+			lastItem.setNext(addedNode);
+			lastItem = addedNode;
+		}
+		listSize = listSize + 1;
+		return objReturned;
+		
 	}
 }
